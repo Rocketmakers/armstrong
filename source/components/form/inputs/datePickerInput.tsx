416d62628c8829f7c2e15f10162cc3cc8d15535a
Jonny Stoten <jonny@rocketmakers.com>
2016-07-01 10:28:57 +0100

    Tidy up a bit

--- source/components/form/inputs/datePickerInput.tsx
+++ source/components/form/inputs/datePickerInput.tsx
@@ -47,20 +47,22 @@ export class DatePickerInput extends React.Component<IDatePickerInputProps, IDat
     date: moment().startOf('day'),
     locale: 'en-gb'
   }
+
   constructor(props: IDatePickerInputProps) {
     super(props);
-    var todaysDate = moment().startOf('day');
+    const todaysDate = moment().startOf('day');
     this.state = { displayedDate: todaysDate.clone(), selectedDate: todaysDate, pickerBodyVisible: false, showOnTop: false };
     moment.locale(props.locale);
     const seed = Math.random().toFixed(4);
     this.inputElementId = "date-picker-text-input-" + seed;
     this.bodyElementId = "date-picker-body-" + seed;
   }
+
   onDaySelected(date: moment.Moment) {
     if (!this.fallsWithinRange(date)) {
       return;
     }
-    var newDate = date.clone();
+    const newDate = date.clone();
     if (this.inputElement) {
       this.inputElement.value = newDate.format(this.format);
     }
@@ -69,10 +71,12 @@ export class DatePickerInput extends React.Component<IDatePickerInputProps, IDat
       this.props.onDateChanged(this.props.returnString ? newDate.format(isoFormat) : newDate.clone());
     }
   }
+
   isEndOfMonth(date: moment.Moment): boolean {
-    var endOfMonth = date.clone().endOf('month');
+    const endOfMonth = date.clone().endOf('month');
     return endOfMonth.isSame(date, 'day');
   }
+
   fallsWithinRange(date: moment.Moment) {
     if (this.props.min && date.isBefore(this.getMinMaxAsMoment(this.props.min), 'day')) {
       return false;
@@ -82,34 +86,36 @@ export class DatePickerInput extends React.Component<IDatePickerInputProps, IDat
     }
     return true;
   }
-  getDaysInMonth() {
-    var days = [];
-    var a = this.state.displayedDate.clone().startOf('month').startOf('day');
-    var b = a.clone().endOf('month');
-    var firstDay = false;
 
-    for (var m = moment(a); m.isBefore(b); m.add(1, 'days')) {
+  getDaysInMonth() {
+    const days = [];
+    const a = this.state.displayedDate.clone().startOf('month').startOf('day');
+    const b = a.clone().endOf('month');
+    let firstDay = false;
+
+    for (const m = moment(a); m.isBefore(b); m.add(1, 'days')) {
       if (!firstDay) {
         firstDay = true;
-        var firstDayIndex = m.weekday();
-        for (var i = firstDayIndex; i > 0; i--) {
+        const firstDayIndex = m.weekday();
+        for (let i = firstDayIndex; i > 0; i--) {
           days.push(this.getDayComponent(true, this.onDaySelected.bind(this), m.clone().subtract(i, 'days')));
         }
       }
       days.push(this.getDayComponent(false, this.onDaySelected.bind(this), m.clone()));
       if (this.isEndOfMonth(m)) {
-        var lastDayIndex = m.weekday();
-        for (var i2 = 1; i2 < 7 - lastDayIndex; i2++) {
-          days.push(this.getDayComponent(true, this.onDaySelected.bind(this), m.clone().add(i2, 'days')));
+        const lastDayIndex = m.weekday();
+        for (let i = 1; i < 7 - lastDayIndex; i++) {
+          days.push(this.getDayComponent(true, this.onDaySelected.bind(this), m.clone().add(i, 'days')));
         }
       }
     }
     return days;
   }
+
   getDayComponent(notInCurrentMonth: boolean, dayClicked: (d: moment.Moment) => void, date: moment.Moment) {
-    var d = date.clone();
-    var dateWithinRange = this.fallsWithinRange(d);
-    var isSelected = d.isSame(this.state.selectedDate, 'day');
+    const d = date.clone();
+    const dateWithinRange = this.fallsWithinRange(d);
+    const isSelected = d.isSame(this.state.selectedDate, 'day');
     return <DatePickerDay
       key={`datepicker_day_${date.format('DDMMYYYY')}`}
       selected={isSelected}
@@ -118,15 +124,17 @@ export class DatePickerInput extends React.Component<IDatePickerInputProps, IDat
       dayClicked={dayClicked}
       date={d}/>;
   }
+
   changeMonth(increment: number) {
     this.setState({ displayedDate: this.state.displayedDate.add(increment, 'months') }, () => {
       this.shouldShowOnTop();
     })
   }
+
   checkDate(dateString: string) {
-    var m = moment(dateString, this.format, false);
+    const m = moment(dateString, this.format, false);
     if (m.isValid() && this.fallsWithinRange(m)) {
-      var formattedDate = m.format(this.format);
+      const formattedDate = m.format(this.format);
       if (this.inputElement) {
         this.inputElement.value = formattedDate;
       }
@@ -140,52 +148,57 @@ export class DatePickerInput extends React.Component<IDatePickerInputProps, IDat
       }
     }
   }
+
   componentWillMount() {
     this.format = this.props.nativeInput ? isoFormat : this.props.format;
     if (this.props.date) {
       if (typeof this.props.date === "string") {
-        var mDate = moment(this.props.date as string, isoFormat);
+        const mDate = moment(this.props.date as string, isoFormat);
         this.setState({ selectedDate: mDate, displayedDate: mDate.clone() });
       }
       else {
-        var pDate = moment((this.props.date as moment.Moment)).startOf('day');
+        const pDate = moment((this.props.date as moment.Moment)).startOf('day');
         pDate.locale(this.props.locale);
         this.setState({ selectedDate: pDate, displayedDate: pDate.clone() });
       }
     }
   }
+
   componentWillUnmount() {
     if (!this.props.nativeInput) {
       window.removeEventListener('mousedown', this);
     }
   }
+
   componentDidMount() {
     this.inputElement = document.getElementById(this.inputElementId);
     this.bodyElement = document.getElementById(this.bodyElementId);
     if (!this.props.nativeInput) {
       window.addEventListener('mousedown', this);
     }
-
   }
+
   handleEvent(e) {
     if (this.mouseDownOnCalendar) {
       return;
     }
     this.setState({ pickerBodyVisible: false });
   }
+
   onInputFocus() {
     this.setState({ pickerBodyVisible: true }, () => {
       this.shouldShowOnTop();
     })
   }
+
   shouldShowOnTop() {
     if (this.props.nativeInput || !this.inputElement) {
       return;
     }
-    var height = this.bodyElement.clientHeight + 50;
-    var visibleBottom = (window.innerHeight + window.scrollY);
-    var inputRect = this.inputElement.getBoundingClientRect();
-    var remainingSpace = Math.abs(visibleBottom - inputRect.bottom);
+    const height = this.bodyElement.clientHeight + 50;
+    const visibleBottom = (window.innerHeight + window.scrollY);
+    const inputRect = this.inputElement.getBoundingClientRect();
+    const remainingSpace = Math.abs(visibleBottom - inputRect.bottom);
 
     if (remainingSpace < height) {
       this.setState({ showOnTop: true })
@@ -193,6 +206,7 @@ export class DatePickerInput extends React.Component<IDatePickerInputProps, IDat
       this.setState({ showOnTop: false })
     }
   }
+
   getMinMaxAsMoment(input: moment.Moment | string): moment.Moment {
     if (typeof input === "string") {
       return moment(input, isoFormat, true)
@@ -200,6 +214,7 @@ export class DatePickerInput extends React.Component<IDatePickerInputProps, IDat
       return input;
     }
   }
+
   getMinMaxAsString(input: moment.Moment | string): string {
     if (typeof input === "string") {
       return input;
@@ -207,15 +222,16 @@ export class DatePickerInput extends React.Component<IDatePickerInputProps, IDat
       return input.format(isoFormat);
     }
   }
+
   render() {
-    var weekdays = _.range(0, 7).map(n => <div className="date-picker-week-day" key={`day_name_${n}`}>{moment().startOf('week').add(n, 'days').format('dd') }</div>)
-    var days = this.getDaysInMonth();
-    var currentDisplayDate = this.state.displayedDate.format("MMMM - YYYY");
-    var classes = classNames("date-picker-body",
+    const weekdays = _.range(0, 7).map(n => <div className="date-picker-week-day" key={`day_name_${n}`}>{moment().startOf('week').add(n, 'days').format('dd') }</div>)
+    const days = this.getDaysInMonth();
+    const currentDisplayDate = this.state.displayedDate.format("MMMM - YYYY");
+    const classes = classNames("date-picker-body",
       cd("date-picker-body-visible", this.state.pickerBodyVisible && !this.props.alwaysShowCalendar),
       cd("date-picker-top", this.state.showOnTop),
       cd("always-show-calendar", this.props.alwaysShowCalendar));
-    var rootClasses = classNames("date-picker", this.props.className, cd('has-icon', this.props.icon !== null), cd('disabled', this.props.disabled));
+    const rootClasses = classNames("date-picker", this.props.className, cd('has-icon', this.props.icon !== null), cd('disabled', this.props.disabled));
     if (this.props.nativeInput) {
       return (
         <div className={rootClasses}>
@@ -273,7 +289,7 @@ interface IDatePickerDayProps extends React.Props<DatePickerDay> {
 
 class DatePickerDay extends React.Component<IDatePickerDayProps, {}> {
   render() {
-    var classes = classNames(cd('not-in-month', this.props.notInCurrentMonth), cd('selected-day', this.props.selected), cd("day-disabled", !this.props.withinRange));
+    const classes = classNames(cd('not-in-month', this.props.notInCurrentMonth), cd('selected-day', this.props.selected), cd("day-disabled", !this.props.withinRange));
     return <div className={classes} onClick={() => this.props.dayClicked(this.props.date) }>{this.props.date.format('DD') }</div>
   }
 }
