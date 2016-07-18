b1cd5e406770833f7e9763007a5404907774728f
Adam Walker <adam@rocketmakers.com>
2016-07-18 15:48:57 +0100

    fixed up some packages

--- workbench/app/views/test.tsx
+++ workbench/app/views/test.tsx
@@ -2,9 +2,7 @@ import * as React from 'react';
 import { Button } from './../../../source/components/interaction/button';
 import { Grid, Row, Col, SingleColumnRow } from './../../../source/components/layout/grid';
 import { Image } from './../../../source/components/display/image';
-import { VelocityDialog } from './../../../source/components/display/velocityDialog';
 
-import { VelocityComponent, VelocityTransitionGroup } from 'velocity-react';
 
 export class Test extends React.Component<{}, { showSubComponent: boolean }> {
   constructor() {
@@ -13,11 +11,7 @@ export class Test extends React.Component<{}, { showSubComponent: boolean }> {
   }
 
   public render() {
-    var v = VelocityComponent;
     return <Grid fillContainer={true} debugMode={true} style={{ backgroundColor: "red" }}>
-      <VelocityDialog isOpen={this.state.showSubComponent}>
-        <div>Hello world!</div>
-      </VelocityDialog>
       <Row style={{ backgroundColor: "green" }}>
         <Col style={{ backgroundColor: "yellow" }}>
           <Button text="click me" onClick={() => this.setState({ showSubComponent: !this.state.showSubComponent }) }/>
