e7efb71ec844f04d0a7f1a0f99979e8c73daa117
Adam Walker <adam@rocketmakers.com>
2017-11-16 14:40:16 +0000

    fixed classname not applying to autocomplete inputs outside of multiselect mode

--- armstrong-react/source/components/form/inputs/autoCompleteInput.tsx
+++ armstrong-react/source/components/form/inputs/autoCompleteInput.tsx
@@ -27,7 +27,7 @@ export interface IAutoCompleteInputProps extends IFormInputProps<AutoCompleteInp
   /** (string) The text to use a placeholder when no value is present */
   placeholder?: string;
   /** (string) The text show when no results were found */
-  noResultsMessage?: string | JSX.Element | {(value: string): string | JSX.Element};
+  noResultsMessage?: string | JSX.Element | { (value: string): string | JSX.Element };
   /** (IAutoCompleteOption[]) If you are using local rather than remote options, specify them here */
   options?: IAutoCompleteOption[];
   /** (number) How long to wait after every key press before executing a remote query */
@@ -387,7 +387,7 @@ export class AutoCompleteInput extends React.Component<IAutoCompleteInputProps, 
                   className={`autocomplete-select-list${this.state.showOnTop ? ' on-top' : ''}`}
                   style={{ maxHeight: `${(this.props.visibleItems || 3) * this.itemHeight}px`, marginTop: `${this.state.topOffset}px` }}>
                   {this.state.filteredOptions && this.state.filteredOptions.map((o, i) =>
-                    <div data-index={i} key={`dd-item-${i}`} className={`dd-list-item${i === this.state.selectedIndex ? ' selected' : ''}${(this.props.multiSelect && _.some((this.state.selectedValue as IAutoCompleteOption[]), ddo => ddo.id === o.id)) ? ' in-selected-list' : ''}`}
+                    <div data-index={i} key={`dd-item-${i}`} className={`dd-list-item${o.className ? ` ${o.className}` : ''}${i === this.state.selectedIndex ? ' selected' : ''}${(this.props.multiSelect && _.some((this.state.selectedValue as IAutoCompleteOption[]), ddo => ddo.id === o.id)) ? ' in-selected-list' : ''}`}
                       onClick={() => this.handleSelection(o)}>{o.name}</div>)}
                   {this.state.filteredOptions.length === 0 && this.state.query && <div className="dd-list-item-no-select">{getNoResults(this.state.query, this.props.noResultsMessage)}</div>}
                 </div>
@@ -401,11 +401,11 @@ export class AutoCompleteInput extends React.Component<IAutoCompleteInputProps, 
   }
 }
 
-function getNoResults(search: string, message: string | JSX.Element | {(value: string): string | JSX.Element}): string | JSX.Element{
+function getNoResults(search: string, message: string | JSX.Element | { (value: string): string | JSX.Element }): string | JSX.Element {
   if (!message) {
     return "No results..."
   }
-  if (_.isFunction(message)){
+  if (_.isFunction(message)) {
     return message(search)
   }
   return message;
