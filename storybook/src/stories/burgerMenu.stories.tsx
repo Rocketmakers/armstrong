d1e19e69714209b1e9e0a69a595c3a035237c4f3
Ben <ben@rocketmakers.com>
2019-09-06 17:01:55 +0100

    side bar

--- storybook/src/stories/burgerMenu.stories.tsx
+++ storybook/src/stories/burgerMenu.stories.tsx
@@ -1,18 +1,13 @@
 import * as React from "react"
-
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import centered from '@storybook/addon-centered/react';
-import { NewBurgerMenu, Icon } from "../_symlink";
+import { Icon, SimpleBurgerMenu, useSimpleBurgerMenu, Button } from "../_symlink";
 
 import "../theme/theme.scss";
 
 storiesOf('Burger menu', module)
-
-  .addDecorator(centered)
-  .add('hello', () => <NewBurgerMenu
+  .add('Simple', () => <SimpleBurgerMenu
     position={"left"}
-    collapse={true}
+    mode={"push"}
     content={<div>
       <div className="armstrong-burger-menu-item"><Icon icon={Icon.Icomoon.sad} />LINK 1</div>
       <div className="armstrong-burger-menu-item"><Icon icon={Icon.Icomoon.sad} />LINK 2</div>
@@ -22,6 +17,29 @@ storiesOf('Burger menu', module)
     closeButtonIcon={Icon.Icomoon.cross}
     openButtonIcon={Icon.Icomoon.menu3}
   >
-    <div id="wow">BODY</div>
-  </NewBurgerMenu>)
+    <div id="site-wrapper">BODY</div>
+  </SimpleBurgerMenu>)
+  .add('Simple with hook', () =>
+    <SimpleBurgerMenu
+      hideOpenButton={true}
+      content={<div>
+        <div className="armstrong-burger-menu-item"><Icon icon={Icon.Icomoon.sad} />LINK 1</div>
+        <div className="armstrong-burger-menu-item"><Icon icon={Icon.Icomoon.sad} />LINK 2</div>
+        <div className="armstrong-burger-menu-item"><Icon icon={Icon.Icomoon.sad} />LINK 3</div>
+        <div className="armstrong-burger-menu-item"><Icon icon={Icon.Icomoon.sad} />LINK 4</div>
+      </div>}
+      closeButtonIcon={Icon.Icomoon.cross}
+      openButtonIcon={Icon.Icomoon.menu3}
+    >
+      <div id="site-wrapper"><Header /></div>
+    </SimpleBurgerMenu>)
 
+
+const Header: React.FC<{}> = p => {
+  const { open, setOpen } = useSimpleBurgerMenu()
+  return (
+    <>
+      <Button onClick={() => setOpen(!open)}>{open ? "CLOSE" : "OPEN"} MENU</Button>
+    </>
+  )
+}
