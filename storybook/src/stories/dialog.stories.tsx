c566ee6537d2f07b2c127f7dae6d4b65d6f3b7ce
Adam Walker <adam@rocketmakers.com>
2019-09-10 08:57:21 +0100

    minor dialog story fixes

--- storybook/src/stories/dialog.stories.tsx
+++ storybook/src/stories/dialog.stories.tsx
@@ -1,7 +1,7 @@
 import * as React from "react"
 
 import { storiesOf } from '@storybook/react';
-import { Grid, Row, Col, Dialog, TextInput, Button, useDialog } from "../_symlink";
+import { Grid, Row, Col, Dialog, TextInput, Button, useDialog, DateInput } from "../_symlink";
 
 import "../theme/theme.scss";
 
@@ -15,12 +15,13 @@ storiesOf('Dialog', module)
       [
         <Dialog bodySelector="body" isOpen={open} onClose={() => setOpen(false)}>
           <SampleDialogContent />
+          <DateInput />
         </Dialog>,
         <Button onClick={() => setOpen(true)}>Open Classic</Button>
       ]
     )
   })
-  .add('Hook dialog [BROKEN]', () => {
+  .add('Hook dialog', () => {
     const { open, portal } = useDialog(SampleDialogContent, { hostElement: "body" })
     return (
       <>
