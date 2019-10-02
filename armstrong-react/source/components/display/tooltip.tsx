e0768a41e1d8e3548f145757bf0cd7259f67b04f
James Benson <James@Rocketmakers.com>
2019-10-02 17:07:59 +0100

    Turned functions into useCallbacks, fixed small mistakes.

--- armstrong-react/source/components/display/tooltip.tsx
+++ armstrong-react/source/components/display/tooltip.tsx
@@ -8,7 +8,7 @@ type Positions = Position | Position[]
 type PositionPriority = [Position, Position, Position, Position, Position, Position]
 
 export interface ITooltipProps {
-  /** (JSX.Element) The content to appear on hover.  Passing a string will apply it as an aria-label to children, if ariaLabel not passed */
+  /** (JSX.Element | string) The content to appear on hover.  Passing a string will apply it as an aria-label to children, if ariaLabel not passed */
   tooltip: JSX.Element | string
   /** (JSX.Element) The content that causes the tooltip to appear when hovered. */
   children: JSX.Element
@@ -28,10 +28,31 @@ export const Tooltip: React.FC<ITooltipProps> = props => {
   const tooltipElement = React.useRef<HTMLDivElement>(null)
   const [currentPosition, setCurrentPosition] = React.useState(0) // Index in position priority array currently being used
 
+  // Creates full position priority array, by taking user specified positions, & adding unspecified positions onto the end
+  const normalisePositionPriority = React.useCallback((positions: Positions): PositionPriority => {
+    const positionsArray: Position[] = typeof positions === "string" ? [positions] : positions
+    const uniquePositions: Position[] = _.uniq(positionsArray)
+    const unsetPositions: Position[] = _.difference(defaultPositions, uniquePositions)
+    return [...uniquePositions, ...unsetPositions] as PositionPriority
+  }, [])
+
   // Uses default postion priority array, if no position prop is passed
   const positionPriority = React.useMemo(() => {
     return position ? normalisePositionPriority(position) : defaultPositions
   }, [position])
+
+  // Checks if an element is fully within the viewport
+  const isInViewport = React.useCallback((element: Element): boolean => {
+    const bounding = element.getBoundingClientRect()
+    const viewportWidth = document.documentElement.clientWidth || window.innerWidth
+    const viewportHeight = document.documentElement.clientHeight || window.innerHeight
+    return (
+      bounding.top >= 0 &&
+      bounding.left >= 0 &&
+      bounding.right <= viewportWidth &&
+      bounding.bottom <= viewportHeight
+    )
+  }, [])
 
   // Determines aria-label for children props.
   const actualAriaLabel = React.useMemo(() => {
@@ -46,47 +67,19 @@ export const Tooltip: React.FC<ITooltipProps> = props => {
 
   // Determines if tooltip should be aria-hidden
   const ariaHidden = React.useMemo(() => {
-    if (ariaHideTooltip || ariaHideTooltip === false) {
+    if (typeof ariaHideTooltip === "boolean") {
       return ariaHideTooltip
     } else {
-      return false
+      return true
     }
   }, [ariaHideTooltip])
 
-  // Used to triggers the useEffect after this one, when currentPositon is already 0
-  React.useEffect(() => {
-    if (currentPosition === -1) {
-      setCurrentPosition(0)
-    }
-  }, [currentPosition])
-
   // If current tooltip position is not visible, moves on to next position
   React.useEffect(() => {
-    if (tooltipElement.current && !isInViewport(tooltipElement.current) && currentPosition < 5) {
+    if (tooltipElement.current && currentPosition < 5 && (currentPosition < 0 || !isInViewport(tooltipElement.current))) {
       setCurrentPosition(currentPosition + 1)
     }
   }, [tooltipElement, currentPosition])
-
-  // Creates full position priority array, by taking user specified positions, & adding unspecified positions onto the end
-  function normalisePositionPriority(positions: Positions): PositionPriority {
-    const positionsArray: Position[] = typeof positions === "string" ? [positions] : positions
-    const uniquePositions: Position[] = _.uniq(positionsArray)
-    const unsetPositions: Position[] = _.difference(defaultPositions, uniquePositions)
-    return [...uniquePositions, ...unsetPositions] as PositionPriority
-  }
-
-  // Checks if an element is fully within the viewport
-  function isInViewport(element: Element): boolean {
-    const bounding = element.getBoundingClientRect()
-    const viewportWidth = document.documentElement.clientWidth || window.innerWidth
-    const viewportHeight = document.documentElement.clientHeight || window.innerHeight
-    return (
-      bounding.top >= 0 &&
-      bounding.left >= 0 &&
-      bounding.right <= viewportWidth &&
-      bounding.bottom <= viewportHeight
-    )
-  }
 
   return (
     // currentPosition set to -1, as setting to 0 doesn't trigger useEffect when it's already 0
