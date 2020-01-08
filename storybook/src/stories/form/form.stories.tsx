8a424ad495cc295b90da948d72d43fa38d8aa58f
patrickbenjamin1 <crumbleshake@live.co.uk>
2020-01-08 15:40:39 +0000

    added placeholder support to select input

--- storybook/src/stories/form/form.stories.tsx
+++ storybook/src/stories/form/form.stories.tsx
@@ -5,11 +5,12 @@ import { storiesOf } from "@storybook/react";
 import "../../theme/theme.scss";
 import { TextInput } from "../../_symlink";
 import { useForm } from "../../_symlink/components/form/formHooks";
+import { IFormValidationResult } from "../../_symlink/components/form/formCore";
 
 storiesOf("Form", module)
   .addParameters({
     options: {
-      showAddonPanel: true
+      showAddonPanel: false
     }
   })
   .add("Binding / useForm", () => {
@@ -28,9 +29,10 @@ storiesOf("Form", module)
       <>
         <p>
           A stateless data bindable form - state is held within the 'dataBinder'
-          property. It triggers a rerender in the component containing the useForm
-          hook upon each change in a bound form element, allowing the state returned in
-          dataBinder to be used for (for example) API requests.
+          property. It triggers a rerender in the component containing the
+          useForm hook upon each change in a bound form element, allowing the
+          state returned in dataBinder to be used for (for example) API
+          requests.
         </p>
         <p>
           The returned object bind contains methods that return binding props
@@ -82,6 +84,74 @@ storiesOf("Form", module)
         </DataForm>
         <br />
         <p>{JSON.stringify(dataBinder.toJson())}</p>
+      </>
+    );
+  })
+  .add("Validation Errors", () => {
+    const initialData = React.useMemo(
+      () => ({
+        inputOne: "foo",
+        inputTwo: "bar",
+        inputThree: 45
+      }),
+      []
+    );
+
+    const { DataForm, bind } = useForm(initialData);
+
+    const validationErrors = React.useMemo<IFormValidationResult[]>(
+      () => [
+        // {
+        //   attribute: "inputOne",
+        //   message: "Something was wrong with this value"
+        // },
+        {
+          attribute: "inputTwo",
+          message: "Something was wrong with this value"
+        },
+        {
+          attribute: "inputThree",
+          message: "Something was wrong with this value"
+        }
+      ],
+      []
+    );
+
+    return (
+      <>
+        <p>
+          Validation errors are passed into the <strong>DataForm </strong>
+          component to enable easy to handle form validation warnings on
+          Armstrong input elements, bound using the <strong>useForm </strong>
+          hook
+        </p>
+        <p>
+          These errors must be passed into the validationResults prop of the
+          <strong> DataForm</strong> component, and must be and array of objects
+          shaped as follows
+        </p>
+
+        <pre>
+          {`[
+  {
+    attribute: "THE KEY OF THE ATTRIBUTE IN THE BINDER",
+    message: "THE MESSAGE TO DISPLAY IN VALIDATION"
+  }
+]`}
+        </pre>
+
+        <br />
+        <br />
+
+        <DataForm validationResults={validationErrors} validationMode="below">
+          <TextInput {...bind.text("inputOne")} />
+        </DataForm>
+        <DataForm validationResults={validationErrors} validationMode="icon">
+          <TextInput {...bind.text("inputTwo")} />
+        </DataForm>
+        <DataForm validationResults={validationErrors} validationMode="both">
+          <TextInput {...bind.text("inputThree")} />
+        </DataForm>
       </>
     );
   });
