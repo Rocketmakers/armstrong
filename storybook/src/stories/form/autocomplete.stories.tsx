efd753fbda426c4775d135ee916652eaae7fbdd5
patrickbenjamin1 <crumbleshake@live.co.uk>
2020-01-17 17:11:09 +0000

    added confirm dialog option to buttons, written story for this

--- storybook/src/stories/form/autocomplete.stories.tsx
+++ storybook/src/stories/form/autocomplete.stories.tsx
@@ -1,22 +1,59 @@
-import * as React from "react"
-import { useOptions, IAutoCompleteOption, AutoCompleteSingleInput, Icon } from "../../_symlink";
+import * as React from "react";
+import {
+  useOptions,
+  IAutoCompleteOption,
+  AutoCompleteSingleInput,
+  Icon
+} from "../../_symlink";
 
-import { storiesOf } from '@storybook/react';
-
+import { storiesOf } from "@storybook/react";
 
 import "../../theme/theme.scss";
+import { AutoCompleteMultiInput } from "../../_symlink/components/form/inputs/autoCompleteMultiInput";
 
-storiesOf('Form/AutocompleteInput', module)
+storiesOf("Form/AutocompleteInput", module)
   .addParameters({
     options: {
       showAddonPanel: true
     }
   })
-  .add('Standard', () => {
-    const { options, filter, setFilter } = useOptions([{ id: 1, name: "1" }, { id: 2, name: "2" }, { id: 3, name: "3" }])
-    const [value, onValueChange] = React.useState<IAutoCompleteOption>(undefined)
+  .add("Standard", () => {
+    const { options, filter, setFilter } = useOptions([
+      { id: 1, name: "1" },
+      { id: 2, name: "2" },
+      { id: 3, name: "3" }
+    ]);
+    const [value, onValueChange] = React.useState<IAutoCompleteOption>(
+      undefined
+    );
     return (
-      <AutoCompleteSingleInput leftIcon={Icon.Icomoon.search} value={value} onValueChange={onValueChange} options={options} filter={filter} onFilterChange={setFilter} />
-    )
-  }
-  )
+      <AutoCompleteSingleInput
+        leftIcon={Icon.Icomoon.search}
+        value={value}
+        onValueChange={onValueChange}
+        options={options}
+        filter={filter}
+        onFilterChange={setFilter}
+      />
+    );
+  })
+  .add("Multiple", () => {
+    const { options, filter, setFilter } = useOptions([
+      { id: 1, name: "1" },
+      { id: 2, name: "2" },
+      { id: 3, name: "3" }
+    ]);
+    const [value, onValueChange] = React.useState<IAutoCompleteOption[]>(
+      []
+    );
+    return (
+      <AutoCompleteMultiInput
+        leftIcon={Icon.Icomoon.search}
+        value={value}
+        onValueChange={onValueChange}
+        options={options}
+        filter={filter}
+        onFilterChange={setFilter}
+      />
+    );
+  });
